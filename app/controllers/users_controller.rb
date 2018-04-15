b758325d1fa3c0572dd55ac05b9272a3906a3355
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-15 12:55:05 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
+  get '/signup' do #sign up the user, if a user isn't logged in
     if !logged_in?
       erb :"users/signup"
     else
@@ -8,14 +8,23 @@ class UsersController < ApplicationController
     end
   end
 
-  post '/signup' do
+  post '/signup' do #create a new user with inputs or send them back to sign up page if any field is blank
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect '/signup'
     else
       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect '/tweets'
     end
   end
+
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirct '/tweets'
+    end
+  end
+
 
 end

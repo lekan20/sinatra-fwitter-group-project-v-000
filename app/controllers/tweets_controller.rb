7fb5db415924102e5e47d10443c8145bfd45609d
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-15 12:46:31 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   get '/tweets' do #shows all tweets
     if logged_in?
       @tweets = Tweet.all
-      erb :"tweets/tweets"
+      erb :"/tweets/tweets"
     else
       redirect "/login"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+
 end
